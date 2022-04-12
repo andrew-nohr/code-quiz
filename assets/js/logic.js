710ca9136f3abf35c524c266afd6b7ce00c4923e
goingblindbro <NOHRENTERPRISE@GMAIL.COM>
2022-04-11 19:28:50 -0700

    created topscore.html, JS variables, and started on functions.

---
+++ assets/js/logic.js
@@ -0,0 +1,45 @@
+// variables keep track of quiz
+var currentQuestionIndex
+var time = questions.length * 15
+var timerId;
+
+// variables reference DOM
+var questionsEl = docurment.getElementById("questions");
+var timerEL = document.getElementById("time");
+var choicesEl = documen.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var initialsEl = document.getElementById("initials");
+var feedbackEl = document.getElementById("feedback");
+
+function startQuiz() {
+// hide start screen
+    var startScreenEl = document.getElementById("start-screen");
+}
+
+// reveal question section
+questionsEl.removeAttribute("class");
+
+// starting time
+timerEL.textContent = time;
+
+getQuestion();
+
+function getQuestion() {
+    // get question from array
+    var currentQuestion = questions[currentQuestionIndex];
+
+    // update update title to question
+    var titleEl = document.getElementById("question-title");
+    titleEl.textContent = currentQuestion.title;
+
+    // clear old answers
+    choicesEl.innerHTML = "";
+
+    // loop choices
+    currentQuestion.choices.forEach(function(choice, i) {
+        var choiceNode = documen.createElement("button");
+        choiceNode.setAttribute("calss", "choice");
+        choiceNode.setAttribute("value", choice);
+    });
+}
